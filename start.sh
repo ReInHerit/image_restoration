cf981b403a8d9f9b3ca0948bcc8f0e6d526111e6
Filippo Principi <arkfil@gmail.com>
2023-04-14 19:35:53 +0200

    FIX: new Dockerfile + start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 echo 'starting Django server on port ' $PORT
-# turn on bash's job control
-python /app/manage.py runserver 0.0.0.0:$PORT &
-sleep 5
-#
-echo 'starting Flask server on port' $FLASK_PORT
-python /app/bring_to_life.py
-
+python /app/manage.py runserver 0.0.0.0:$PORT
 echo 'all servers started'
 
